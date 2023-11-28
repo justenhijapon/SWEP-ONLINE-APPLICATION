8a6c5a28f03d6c48eac953f1f2ce8d6fe092142d
MISVIS <sramisvis@gmail.com>
2023-11-28 15:24:23 +0800

    remove required on project code and amount fields in GST and other activities

--- app/Http/Requests/OtherActivities/OtherActivitiesFormRequest.php
+++ app/Http/Requests/OtherActivities/OtherActivitiesFormRequest.php
@@ -17,8 +17,8 @@ class OtherActivitiesFormRequest extends FormRequest
             'activity' => 'required|string|max:255',
             'venue' => 'required|string|max:255',
             'details' => 'nullable|string|max:1000',
-            'project_code' => 'required|string|max:45|exists:projects,project_code',
-            'utilized_fund' => 'required|string|max:20',
+//            'project_code' => 'required|string|max:45|exists:projects,project_code',
+//            'utilized_fund' => 'required|string|max:20',
             'date' => 'required|date_format:"Y-m-d"',
         ];
 
